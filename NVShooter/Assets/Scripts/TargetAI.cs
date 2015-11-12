33c9f58929b4cd705e55018d713351857d4dcfd6
sandrockman <svc-student@RC335G05.student.directory.jccc>
2015-11-12 08:57:49 -0600

    new additions

---
+++ NVShooter/Assets/Scripts/TargetAI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TargetAI : MonoBehaviour {
+
+	public Collider TargetActiveRange;
+	public float moveSpeed;
+	public float rotateSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Idle ()
+	{
+
+	}
+
+	void Wander()
+	{
+
+	}
+
+	void Circle()
+	{
+
+	}
+
+	void Avoid()
+	{
+
+	}
+
+	void Seek ()
+	{
+
+	}
+}
