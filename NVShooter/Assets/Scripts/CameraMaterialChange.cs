4c0b50363cd9518cc735b2cf33b566723489f9f1
sandrockman <vhaskin1@stumail.jccc.edu>
2015-12-08 01:28:30 -0600

    menu and started extra networking

---
+++ NVShooter/Assets/Scripts/CameraMaterialChange.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMaterialChange : MonoBehaviour {
+
+    public Material[] materialList;
+    public GameObject[] parts;
+
+    public CameraColors cameraColor;
+
+    public enum CameraColors
+    {
+        BLUE,
+        INDIGO,
+        PURPLE,
+        RED,
+        ORANGE,
+        YELLOW,
+        GREEN,
+        WHITE,
+        BLACK,
+        BROWN
+    };
+
+	// Use this for initialization
+	void Start () {
+        //parts = gameObject.GetComponentsInChildren<GameObject>();
+
+        ChangeColor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    public void ChangeColor()
+    {
+        foreach (GameObject part in parts)
+        {
+            Renderer render = part.GetComponent<Renderer>();
+            switch (cameraColor)
+            {
+                case CameraColors.BLACK:
+                    render.material = materialList[(int)CameraColors.BLACK];
+                    break;
+                case CameraColors.BLUE:
+                    render.material = materialList[(int)CameraColors.BLUE];
+                    break;
+                case CameraColors.BROWN:
+                    render.material = materialList[(int)CameraColors.BROWN];
+                    break;
+                case CameraColors.GREEN:
+                    render.material = materialList[(int)CameraColors.GREEN];
+                    break;
+                case CameraColors.INDIGO:
+                    render.material = materialList[(int)CameraColors.INDIGO];
+                    break;
+                case CameraColors.ORANGE:
+                    render.material = materialList[(int)CameraColors.ORANGE];
+                    break;
+                case CameraColors.PURPLE:
+                    render.material = materialList[(int)CameraColors.PURPLE];
+                    break;
+                case CameraColors.RED:
+                    render.material = materialList[(int)CameraColors.RED];
+                    break;
+                case CameraColors.WHITE:
+                    render.material = materialList[(int)CameraColors.WHITE];
+                    break;
+                case CameraColors.YELLOW:
+                    render.material = materialList[(int)CameraColors.YELLOW];
+                    break;
+                default:
+                    Debug.Log("Problem with setting camera part color.");
+                    break;
+            }
+        }
+    }
+}
---
+++ NVShooter/Assets/Scripts/NewMainMenuScript.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System;
+
+public class NewMainMenuScript : NetworkBehaviour {
+
+    enum CanvasSetting
+    {
+        MAIN,
+        CREATE,
+        JOIN
+    };
+
+    public Canvas mainMenu;
+    public Canvas createMenu;
+    public Canvas joinMenu;
+
+    CanvasSetting menuSetting;
+
+    public NetworkManager manager;
+    public Text levelSelect;
+
+	// Use this for initialization
+	void Start () {
+        menuSetting = CanvasSetting.MAIN;
+	}
+
+    public void _MenuCreateGame()
+    {
+        createMenu.enabled = true;
+        mainMenu.enabled = false;
+        menuSetting = CanvasSetting.CREATE;
+    }
+
+    public void _MenuJoinGame()
+    {
+        joinMenu.enabled = true;
+        mainMenu.enabled = false;
+        menuSetting = CanvasSetting.JOIN;
+    }
+
+    public void _QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void _BackToMainMenu()
+    {
+        mainMenu.enabled = true;
+        createMenu.enabled = false;
+        joinMenu.enabled = false;
+        menuSetting = CanvasSetting.MAIN;
+    }
+
+    public void _CreateHostGame()
+    {
+        manager.StartHost();
+    }
+
+    public void _JoinHostGame()
+    {
+        manager.StartClient();
+    }
+
+    public void _UpdateHostName(InputField newHostname)
+    {
+        if(String.IsNullOrEmpty(newHostname.text))
+        {
+            manager.networkAddress = "localhost";
+        }
+        else
+        {
+            manager.networkAddress = newHostname.text;
+        }
+    }
+
+    public void _UpdatePortNumber(InputField intStep)
+    {
+        int newPort;
+        if (Int32.TryParse(intStep.text, out newPort))
+        {
+            manager.networkPort = newPort;
+        }
+        else
+        {
+            manager.networkPort = 7777;
+        }
+    }
+
+    public void _LevelChange()
+    {
+        if(manager.onlineScene == "Level1")
+        {
+            manager.onlineScene = "Level2";
+            levelSelect.text = "Level 2";
+        }
+        else
+        {
+            manager.onlineScene = "Level1";
+            levelSelect.text = "Level 1";
+        }
+    }
+}
