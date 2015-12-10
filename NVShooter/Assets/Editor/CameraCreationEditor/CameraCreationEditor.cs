8e96192f06605c910a4171f5826fb7c84c95183e
sandrockman <vhaskin1@stumail.jccc.edu>
2015-12-10 12:11:12 -0600

    level two path. camera issues. start tool creation
    camera local transform pulls away from player to match start location on
    global transform.
    trouble getting any tool stuff deciphered...again.

---
+++ NVShooter/Assets/Editor/CameraCreationEditor/CameraCreationEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+using System.IO;
+
+public class CameraCreationEditor : MonoBehaviour {
+
+    [MenuItem("Camera Creation/Create Camera")]
+    public static void CreateCamera()
+    {
+
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
